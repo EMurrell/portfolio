951c49e21db29d50eab5340aed862f4fa41b34a2
EMurrell <82235988+EMurrell@users.noreply.github.com>
2021-07-23 10:52:33 -0400

    fix x-overflow

--- components/Contact.js
+++ components/Contact.js
@@ -11,40 +11,34 @@ export default function About() {
         id="Contact"
         className="flex flex-col items-center justify-center w-full bg-mybeige"
       >
-        {/* <div className="flex flex-row "> */}
-        {/* <div className="w-1/6 h-screen border-r-2 border-myblack">
-            <p className="text-base font-extrabold tracking-wider -rotate-90 mt-60 md:text-lg lg:text-xl text-myorange font-headline">
-              CONTACT
-            </p>
-          </div> */}
         <div>
           <h2 className="justify-center px-12 pt-24 pb-2 text-lg font-extrabold tracking-wide text-center md:pt-32 font-headline md:text-2xl lg:text-3xl text-myorange">
             FEEL FREE TO GET IN TOUCH:
           </h2>
         </div>
 
-        <div className="flex flex-col justify-center p-12 pb-24 text-center md:flex-row ">
-          <div className="flex-col px-12 py-4 ">
-            <h2 className="text-base font-extrabold tracking-wide transition duration-500 ease-in-out transform md:text-xl lg:text-2xl font-headline text-myblack hover:text-myblue hover:-translate-y-1 hover:scale-110">
+        <div className="flex flex-col justify-center p-10 pb-24 text-center md:flex-row ">
+          <div className="flex-col px-5 py-4 ">
+            <h2 className="text-base font-extrabold tracking-wide transition duration-500 ease-in-out transform md:text-lg lg:text-2xl font-headline text-myblack hover:text-myblue hover:-translate-y-1 hover:scale-110">
               EMAIL
             </h2>
-            <p className="text-sm font-bold tracking-wide md:text-lg lg:text-xl font-body text-midtone">
+            <p className="text-sm font-bold tracking-wide md:text-sm lg:text-lg font-body text-midtone">
               hello@EMurrell.com
             </p>
           </div>
-          <div className="flex-col px-12 py-4 text-base font-extrabold tracking-wide hover:text-myblue">
-            <h2 className="text-base font-extrabold tracking-wide transition duration-500 ease-in-out transform md:text-xl lg:text-2xl font-headline text-myblack hover:text-myblue hover:-translate-y-1 hover:scale-110">
+          <div className="flex-col px-5 py-4 text-base font-extrabold tracking-wide hover:text-myblue">
+            <h2 className="text-base font-extrabold tracking-wide transition duration-500 ease-in-out transform md:text-lg lg:text-2xl font-headline text-myblack hover:text-myblue hover:-translate-y-1 hover:scale-110">
               <a href="https://github.com/EMurrell">GITHUB</a>
             </h2>
-            <p className="text-sm font-bold tracking-wide md:text-lg lg:text-xl font-body text-midtone">
+            <p className="text-sm font-bold tracking-wide md:text-sm lg:text-lg font-body text-midtone">
               github.com/EMurrell
             </p>
           </div>
-          <div className="flex-col px-12 py-4 text-base font-extrabold tracking-wide hover:text-myblue">
-            <h2 className="text-base font-extrabold tracking-wide transition duration-500 ease-in-out transform md:text-xl lg:text-2xl font-headline text-myblack hover:text-myblue hover:-translate-y-1 hover:scale-110">
+          <div className="flex-col px-5 py-4 text-base font-extrabold tracking-wide hover:text-myblue">
+            <h2 className="text-base font-extrabold tracking-wide transition duration-500 ease-in-out transform md:text-lg lg:text-2xl font-headline text-myblack hover:text-myblue hover:-translate-y-1 hover:scale-110">
               <a href="https://twitter.com/MurrellWeb">TWITTER</a>
             </h2>
-            <p className="text-sm font-bold tracking-wide md:text-lg lg:text-xl font-body text-midtone">
+            <p className="text-sm font-bold tracking-wide md:text-sm lg:text-lg font-body text-midtone">
               twitter.com/MurrellWeb
             </p>
           </div>
--- components/Work.js
+++ components/Work.js
@@ -2,46 +2,45 @@ import Link from "next/link";
 
 export default function Work() {
   return (
-    <section id="Work" className="flex flex-col w-full h-screen bg-myblack">
+    <section id="Work" className="flex flex-col w-full bg-myblack">
       <div className="flex flex-row ">
-        <div className="flex flex-col justify-center w-5/6 h-screen border-r-2 border-mybeige">
+        <div className="flex flex-col justify-center w-5/6 border-r-2 border-mybeige">
           <div className="flex flex-col py-4 lg:flex-row">
-            <div className="px-4 lg:pl-24">
+            <div className="flex justify-center pl-4 lg:pl-24">
               <img
                 src="ptp-thumb.png"
                 alt="ptp case study"
-                height={500}
-                width={500}
+                height={700}
+                width={700}
               />
             </div>
-            <div className="flex flex-col justify-center px-16 py-4">
-              <h1 className="self-center p-4 text-sm font-extrabold text-center md:text-base text-mybeige font-headline">
+            <div className="flex flex-col justify-center text-center lg:text-left">
+              <h1 className="py-2 pl-4 text-sm font-extrabold md:text-base text-mybeige font-headline">
                 PUP TO PAL <br />
-                DOG TRAINING
               </h1>
               <Link href="/ptp">
-                <a className="self-center text-sm font-extrabold tracking-wide transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-body text-myorange hover:text-myblue">
+                <a className="pb-6 pl-4 text-sm font-extrabold tracking-wide transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-body text-myorange hover:text-myblue">
                   VIEW CASE STUDY
                 </a>
               </Link>
             </div>
           </div>
           <div className="flex flex-col py-4 lg:flex-row">
-            <div className="px-4 lg:pl-24">
+            <div className="flex justify-center pl-4 lg:pl-24">
               <img
                 src="mtc-thumb.png"
                 alt="mtc case study"
-                height={500}
-                width={500}
+                height={700}
+                width={700}
               />
             </div>
-            <div className="flex flex-col justify-center px-16 ">
-              <h1 className="self-center p-4 text-sm font-extrabold text-center md:text-base text-mybeige font-headline">
-                MASON'S TOUCH <br />
-                CONSTRUCTION
+            <div className="flex flex-col justify-center text-center lg:text-left ">
+              <h1 className="py-2 pl-4 text-sm font-extrabold md:text-base text-mybeige font-headline">
+                MASON'S <br />
+                TOUCH <br />
               </h1>
               <Link href="/mtc">
-                <a className="self-center text-sm font-extrabold tracking-wide transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-body text-myorange hover:text-myblue">
+                <a className="pb-6 pl-4 text-sm font-extrabold tracking-wide transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-body text-myorange hover:text-myblue">
                   VIEW CASE STUDY
                 </a>
               </Link>
@@ -49,7 +48,7 @@ export default function Work() {
           </div>
         </div>
 
-        <div className="w-1/6 h-screen pt-64 pr-4 ">
+        <div className="w-1/6 pt-64 pr-4 ">
           <p className="text-base font-extrabold tracking-wider rotate-90 md:text-lg lg:text-xl text-myorange font-headline">
             WORKS
           </p>
